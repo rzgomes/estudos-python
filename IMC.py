c4630ca904737801cc8fa1c4d17962015baeffbe
rzgomes <souza.raulg@gmail.com>
2025-12-11 23:46:14 -0300

    Desafio IMC

---
+++ IMC.py
@@ -0,0 +1,23 @@
+"""
+Anna Júlia está criando um sistema para calcular o Índice de Massa Corporal (IMC) e fornecer recomendações básicas. O programa deve receber o peso 
+e a altura de uma pessoa e exibir o valor do IMC, além de indicar se está abaixo do peso, com peso normal ou acima do peso. Crie um programa que 
+receba o peso (em kg) e a altura (em metros) e calcule o IMC usando a fórmula: IMC = peso / (altura ** 2) Depois, exiba o valor do IMC e uma mensagem 
+indicando se está abaixo do peso (IMC < 18.5), peso normal (18.5 <= IMC < 25) ou acima do peso (IMC >= 25).
+"""
+
+nome = "Anna Júlia"
+altura = 1.58
+peso = 100
+
+#operação
+
+imc = peso/(altura**2)
+
+print(f"Paciente {nome}")
+print(f"Seu IMC é: {imc:.2f}")
+if imc < 18.5:
+    print("Abaixo do peso")
+elif 18.5 <= imc < 25:
+    print("Peso normal")
+else:
+    print("Acima do peso")

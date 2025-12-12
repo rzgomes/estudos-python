b5700d76c798e5f9f964587c9737fb1e37b902e8
rzgomes <souza.raulg@gmail.com>
2025-12-11 23:26:29 -0300

    meu primeiro programa

--- teste.py
+++ teste.py
@@ -1,1 +1,26 @@
 #comentário de linha
+""" 
+comentário de várias linhas
+todo programa vai ter essas 3 etapas: 1° Input do usuário - Conseguir informações; 2° Processamento; 3° Output
+"""
+print("Hello world")
+
+#ex de variáveis
+
+nome = "Shaolin Soares Leite"
+nota1 = 3
+nota2 = 5
+nota3  = 4
+#exemplo de uma operação mat
+media = (nota1+nota2+nota3)/3
+
+
+#ex de condicionais
+print(f"Aluno: {nome}")
+if media >= 7:
+    print("Aprovado")
+elif 5 <= media <7:
+    print("Recuperação")
+else:
+    print("Reprovado")
+
